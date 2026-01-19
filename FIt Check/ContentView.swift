ae5d52fc5dc4bc695df1dd6f558c7e6081f03ec2
KevilBTN <kevil@beveragetradenetwork.com>
2026-01-19 14:43:30 +0530

    Initial Commit

---
+++ FIt Check/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FIt Check
+//
+//  Created by Kevil Darji on 19/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FIt Check/FIt_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIt_CheckApp.swift
+//  FIt Check
+//
+//  Created by Kevil Darji on 19/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FIt_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
